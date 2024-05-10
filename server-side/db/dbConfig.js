62fd28f9c4547e909ea442ce7583f3fe80fe0d56
RobelGebrekidan2 <146842620+RobelGebrekidan2@users.noreply.github.com>
2024-05-10 16:58:59 +0300

    Change local database tp remote database

--- server-side/db/dbConfig.js
+++ server-side/db/dbConfig.js
@@ -2,10 +2,9 @@ const mysql2 = require("mysql2");
 const dbconnection = mysql2.createPool({
   user: process.env.USER,
   database: process.env.DATABASE,
-  host: "localhost",
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   connectionLimit: 10,
-
 });
 
 module.exports = dbconnection.promise();
